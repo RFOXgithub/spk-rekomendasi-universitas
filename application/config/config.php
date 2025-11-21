60a48a887f127152bdd23455dd2dfc9750128d9b
RFOXgithub <rivalmanti399@gmail.com>
2025-11-21 19:20:51 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']    = 'http://localhost/Sistem-Rekomendasi-Universitas-Terbaik-di-Indonesia/';
+$config['base_url']    = 'http://localhost/spk-rekomendasi-universitas/';
 
 /*
 |--------------------------------------------------------------------------
