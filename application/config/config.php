4d7f625b4983cfe928a083df6df0a243b7b57549
RFOXgithub <rivalmanti399@gmail.com>
2025-11-22 17:47:21 +0700

    UPDATE

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']    = 'http://localhost/spk-rekomendasi-universitas/';
+$config['base_url']    = 'http://localhost/spk-university-recommendation/';
 
 /*
 |--------------------------------------------------------------------------
